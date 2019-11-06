099dc11c84816f9b0b9dc9cec3d311408c52b63b
Francesco Ruvolo <ruvolof@gmail.com>
2019-11-06 13:31:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Image zoomer
+
+## Installation
+
+Simply copy the `imgzoom.js` file on your website, include it in your page and set `<body onload='imgzoom()'`.
+
+An example is available in `test.html`.
+
+This script is in use at [https://werebug.com](https://werebug.com).
---
+++ imgzoom.js
@@ -0,0 +1,48 @@
+function imgzoom() {
+  var images = document.getElementsByTagName('img');
+  for (var i = 0; i < images.length; i++) {
+    var url = images[i].src;
+    images[i].onclick = function() { zoom(url) };
+  }
+}
+
+function zoom(url) {
+  var overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+  overlay.style.position = 'fixed';
+  overlay.style.height = '100%';
+  overlay.style.top = '0';
+  overlay.style.left = '0';
+  overlay.style.right = '0';
+  overlay.style.bottom = '0';
+  overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  overlay.style.zIndex = '2';
+  overlay.style.margin = 'auto';
+  overlay.style.textAlign = 'center';
+  overlay.onclick = function() { removeOverlays() }
+
+  var image = document.createElement('img');
+  image.src = url;
+  image.style.maxHeight = '90%';
+  image.style.margin = 'auto';
+  overlay.appendChild(image);
+
+  document.body.appendChild(overlay);
+
+  document.addEventListener('keydown', escapeListener);
+}
+
+function removeOverlays() {
+  console.log('fored');
+  overlays = document.getElementsByClassName('overlay');
+  for (var i = 0; i < overlays.length; i++) {
+    overlays[i].remove();
+  }
+  document.removeEventListener('keydown', escapeListener);
+}
+
+var escapeListener = function(e) {
+  if (e.key == 'Escape') {
+    removeOverlays();
+  }
+}
